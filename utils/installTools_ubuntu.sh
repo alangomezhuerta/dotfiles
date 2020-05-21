8c805ca080bd58f13aff6122397c3b89681b8b29
alangomezhuerta <alangomezhuerta@email.com>
2020-05-20 23:14:40 -0500

    installTools_ubuntu.sh added

---
+++ utils/installTools_ubuntu.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+
+main() {
+	if [ $1 ]; then
+		apt-get --yes install
+		apt-get --yes upgrade
+
+		if [ $1 = "zsh" ]; then
+			zsh_pack
+		elif [ $i = "java" ]; then
+			java_pack
+		elif [ $i = "perl" ]; then
+			perl_pack
+		elif [ $i = "python2" ]; then
+			python2_pack
+		elif [ $i = "python3" ]; then 
+			python3_pack
+		fi
+		
+	fi
+	exit 0		
+
+}
+
+zsh_pack() {
+	sudo apt-get --yes install zsh fasd fd-find firewalld fzf git-extras httpie nmap ripgrep tig direnv
+}
+
+java_pack() {
+	sudo apt-get --yes install default-jre
+}
+
+perl_pack() {
+	sudo apt-get --yes install perl-doc
+}
+
+python2_pack() {
+	sudo apt-get --yes install python pep8 python-pip python-pip-whl pylint pipenv virtualenv virtualenvwrapper
+}
+
+python3_pack() {
+	sudo apt-get --yes install python3-pip  python3-pylint-django python3-pylint-flask python3-virtualenv python3-virtualenvwrapper
+}
