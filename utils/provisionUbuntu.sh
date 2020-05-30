486b399d863e3b90d35cdc85de4567061c70c816
alangomezhuerta <alangomezhuerta@email.com>
2020-05-30 14:42:23 -0500

    added vagrantfile and provisinUbuntu script

---
+++ utils/provisionUbuntu.sh
@@ -0,0 +1,65 @@
+line=""
+n=0
+
+loopAccounts() {
+	for line in "alan" "ansible";do 
+		n=$((n+1))
+		createAccount 
+		getsshKey
+		if [ $n -eq 1 ]; then
+			createDoDir
+		fi
+	done
+	updateSystem
+	installTools
+	sudoersAllow
+}
+
+
+# For development goals all these accounts are provided with sudo rights
+createAccount() {
+	useradd -d /home/$line -m -c "$line account" -U -G sudo -s /bin/bash $line
+	mkdir /home/$line/.ssh
+	chmod 700 /home/$line/.ssh
+	touch /home/$line/.ssh/authorized_keys
+	chmod 600 /home/$line/.ssh/authorized_keys
+	chown -R $line.$line /home/$line/.ssh
+}
+
+getsshKey() {
+	if [ -f /tmp/key.pub ]; then
+		cat /tmp/key.pub > /home/$line/.ssh/authorized_keys
+	else
+		curl -fLo /tmp/key.pub https://raw.githubusercontent.com/alangomezhuerta/dotfiles/master/utils/wsl01_id_rsa.pub	
+		cat /tmp/key.pub > /home/$line/.ssh/authorized_keys	
+	fi
+}
+
+updateSystem() {
+	apt-get update --yes
+	apt-get upgrade --yes
+}
+
+createDoDir() {
+	mkdir /do && chown $line.$line /do
+}
+
+installTools() {
+	apt-get --yes install net-tools curl wget tree vim mosh aptitude git tmux
+	curl -fLo /tmp/installTools_ubuntu.sh https://raw.githubusercontent.com/alangomezhuerta/dotfiles/master/utils/installTools_ubuntu.sh
+}
+
+sudoersAllow() {
+	# TODO:  adapt this code--> perl -pi.bak -e 's/,tcp,/,udp,/' /etc/sudoers
+	echo "%sudo   ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
+}
+
+echo "+------------------------------------------------+"
+echo "|Script for provisioning is starting             |"
+echo "+------------------------------------------------+"
+
+loopAccounts
+echo "+------------------------------------------------+"
+echo "|Provisioning is done                            |"
+echo "+------------------------------------------------+"
+
